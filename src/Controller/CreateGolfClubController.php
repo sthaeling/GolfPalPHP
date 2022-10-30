41f95ed6981c83c32c8fa3c03a101d095198f89b
Stefan Haeling <stefan.haeling@bestit.de>
2022-10-30 15:50:39 +0100

    Add functionality to add new club

--- src/Controller/CreateGolfClubController.php
+++ src/Controller/CreateGolfClubController.php
@@ -2,18 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\GolfClub;
+use App\Service\GolfClub\CreateGolfClub;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CreateGolfClubController extends AbstractController
 {
+    private CreateGolfClub $createGolfClub;
 
+    /**
+     * @param CreateGolfClub $createGolfClub
+     */
+    public function __construct(CreateGolfClub $createGolfClub)
+    {
+        $this->createGolfClub = $createGolfClub;
+    }
 
     #[Route('/creategolfclub', name: 'app_create_golfclub')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('pages/create_golfClub/index.html.twig', [
-        ]);
+        $golfClub = new GolfClub();
+
+        $form  = $this->createFormBuilder($golfClub)
+            ->add('name', TextType::class)
+            ->add('street', TextType::class)
+            ->add('zipCode', TextType::class)
+            ->add('city', TextType::class)
+            ->add('websiteUrl', TextType::class)
+            ->add('email', TextType::class)
+            ->add('phone', TextType::class)
+            ->add('imageUrl', TextType::class, )
+            ->add('save', SubmitType::class, ['label' => 'Create Club'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $golfClub = $form->getData();
+
+            $response = $this->createGolfClub->createNewGolfClub($golfClub);
+
+            $response = json_decode($response->getContent(), true);
+            return $this->redirectToRoute('app_golf_course', ['golfcourseid' => $response['id']]);
+        }
+
+        return $this->renderForm('pages/create_golfClub/index.html.twig', ['form' => $form]);
     }
 }
---
+++ src/Service/GolfClub/CreateGolfClub.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\GolfClub;
+
+use App\Entity\GolfClub;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Flex\Response;
+
+class CreateGolfClub
+{
+    private ManagerRegistry $managerRegistry;
+
+    /**
+     * @param ManagerRegistry $managerRegistry
+     */
+    public function __construct(ManagerRegistry $managerRegistry)
+    {
+        $this->managerRegistry = $managerRegistry;
+    }
+
+    public function createNewGolfClub(GolfClub $clubData)
+    {
+        $entityManager = $this->managerRegistry->getManager();
+
+        $golfClubRepo = $entityManager->getRepository(GolfClub::class);
+
+        $existingResult = $golfClubRepo->findOneBy(['name' => $clubData->getName()]);
+
+        if ($existingResult) {
+            return new JsonResponse(['id' => $existingResult->getId()], 200);
+        }
+
+        $golfClub = new GolfClub();
+
+        $golfClub->setName($clubData->getName());
+        $golfClub->setStreet($clubData->getStreet());
+        $golfClub->setZipCode($clubData->getZipCode());
+        $golfClub->setCity($clubData->getCity());
+        $golfClub->setEmail($clubData->getEmail());
+        $golfClub->setWebsiteUrl($clubData->getWebsiteUrl());
+        $golfClub->setPhone($clubData->getPhone());
+        $golfClub->setImageUrl($clubData->getImageUrl());
+
+        $entityManager->persist($golfClub);
+
+        $entityManager->flush();
+
+        return new JsonResponse(['id' => $golfClub->getId()], 200);
+    }
+}
